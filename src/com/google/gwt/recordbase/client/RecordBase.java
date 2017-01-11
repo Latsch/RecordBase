3abae19fbdbefa688b3aa31f96f489756a3cc3e5
Sebastian Brossmer <latsch_general@web.de>
2017-01-11 17:17:56 +0100

    Gui Init

--- src/com/google/gwt/recordbase/client/RecordBase.java
+++ src/com/google/gwt/recordbase/client/RecordBase.java
@@ -3,24 +3,31 @@ package com.google.gwt.recordbase.client;
 import com.google.gwt.recordbase.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import java.util.ArrayList;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class RecordBase implements EntryPoint {
+	
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable baseTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox txBox = new TextBox();
+	private Button addButton = new Button("Add");
+	private Label lastUpdateLabel = new Label();
+	private ArrayList<String> record = new ArrayList<String>();
+	
   /**
    * The message displayed to the user when the server cannot be reached or
    * returns an error.
@@ -38,110 +45,83 @@ public class RecordBase implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    final Button sendButton = new Button("Send");
-    final TextBox nameField = new TextBox();
-    nameField.setText("GWT User");
-    final Label errorLabel = new Label();
-
-    // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
-
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
-    RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
-
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
-
-    // Create the popup dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    final Button closeButton = new Button("Close");
-    // We can set the id of a widget by accessing its Element
-    closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-    dialogVPanel.add(closeButton);
-    dialogBox.setWidget(dialogVPanel);
-
-    // Add a handler to close the DialogBox
-    closeButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
-      }
-    });
-
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        sendNameToServer();
-      }
-
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
-      }
-
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void sendNameToServer() {
-        // First, we validate the input.
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
-        
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-            dialogBox.setText("Remote Procedure Call - Failure");
-            serverResponseLabel.addStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(SERVER_ERROR);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-
-          public void onSuccess(String result) {
-            dialogBox.setText("Remote Procedure Call");
-            serverResponseLabel.removeStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(result);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-        });
-      }
-    }
-
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
+	userInterfaceSetup();
+	
+	loadRecordDatabase();
+  }
+  
+  /**
+   * On load Setup of the GUI
+   */
+  public void userInterfaceSetup(){
+	  // Create table for stock data.
+    baseTable.setText(0, 0, "Artist");
+    baseTable.setText(0, 1, "EP Name");
+    baseTable.setText(0, 2, "Release Date");
+    baseTable.setText(0, 3, "Remove");
+	
+	//Adding style names to the Table
+	baseTable.getRowFormatter().addStyleName(0, "watchListHeader");
+	baseTable.addStyleName("watchList");
+	
+	//Adding style names to the price and change header fields
+	baseTable.getCellFormatter().addStyleName(0, 0, "watchListNumericColumn");
+	baseTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
+	baseTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
+	
+	
+	//Assemble AddPanel
+	addPanel.add(txBox);
+	addPanel.add(addButton);
+	addPanel.addStyleName("addPanel");
+	
+	
+	//Centering the mainPanel on the page 
+	mainPanel.setWidth("100%");
+    mainPanel.setHeight("100%");
+    mainPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+    mainPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+    
+	//Adding widgets to the mainPanel
+	mainPanel.add(baseTable);
+	mainPanel.add(addPanel);
+	mainPanel.add(lastUpdateLabel);
+	
+	
+	//Association the Widgets with the HTML page
+	RootPanel.get("recordBase").add(mainPanel); 
+  }
+  
+  
+  /**
+   * Loads the Record data out of a local database (TODO)
+   */
+  public void loadRecordDatabase(){
+	
+	//MOCKUP
+	baseTable.setText(1, 0, "Stevie Wonder");
+    baseTable.setText(1, 1, "Hotter Than July");
+    baseTable.setText(1, 2, "29.9.1980");
+    baseTable.setText(1, 3, "Remove");
+	
+	baseTable.getCellFormatter().addStyleName(1, 0, "watchListNumericColumn");
+	baseTable.getCellFormatter().addStyleName(1, 1, "watchListNumericColumn");
+	baseTable.getCellFormatter().addStyleName(1, 2, "watchListNumericColumn");
+	baseTable.getCellFormatter().addStyleName(1, 3, "watchListRemoveColumn");
+	
+	// Add a button to remove this stock from the table.
+	Button rmButton = new Button("x");
+	rmButton.addStyleDependentName("remove");
+	/*
+	rmButton.addClickHandler(new ClickHandler(){
+		public void onClick(ClickEvent event){
+			int removeIndex = stock.indexOf(symbol);
+			record.remove(removeIndex);
+			baseTable.remove1(removeIndex +1);
+		}
+	});*/
+	baseTable.setWidget(1, 3, rmButton);
   }
 }
+
+	
